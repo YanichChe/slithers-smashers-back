ce6f30f8c12a29ff7dc93ec0caf33e2e0ee58992
tc <ychernovskaya@gmail.com>
2023-10-28 17:27:48 +0700

    Initial commit

---
+++ src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/SlithersSmashersBackApplication.java
@@ -0,0 +1,12 @@
+package ru.ccfit.nsu.chernovskaya.slitherssmashersback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlithersSmashersBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlithersSmashersBackApplication.class, args);
+	}
+}
