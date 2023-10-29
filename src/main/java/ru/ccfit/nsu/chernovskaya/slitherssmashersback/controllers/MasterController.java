b776d1f98d95da3b3c8d1f0591cb07220f886347
tc <ychernovskaya@gmail.com>
2023-10-29 18:16:32 +0700

    add MasterController

---
+++ src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/controllers/MasterController.java
@@ -0,0 +1,52 @@
+package ru.ccfit.nsu.chernovskaya.slitherssmashersback.controllers;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.ccfit.nsu.chernovskaya.slitherssmashersback.SnakesProto;
+import ru.ccfit.nsu.chernovskaya.slitherssmashersback.models.GameInfo;
+import ru.ccfit.nsu.chernovskaya.slitherssmashersback.services.ConnectionService;
+
+@RestController
+@RequestMapping("/master")
+@Log4j2
+public class MasterController {
+
+    private final GameInfo gameInfo;
+    private final ConnectionService connectionService;
+
+    @Autowired
+    public MasterController(GameInfo gameInfo, ConnectionService connectionService) {
+        this.gameInfo = gameInfo;
+        this.connectionService = connectionService;
+    }
+
+    @GetMapping("/start")
+    public void startNewGame(@RequestParam int height,
+                             @RequestParam int width,
+                             @RequestParam int foodStatic,
+                             @RequestParam int stateDelayMs,
+                             @RequestParam String gameName,
+                             @RequestParam String username) {
+
+        SnakesProto.GameConfig gameConfig = SnakesProto.GameConfig
+                .newBuilder()
+                .setHeight(height)
+                .setWidth(width)
+                .setFoodStatic(foodStatic)
+                .setStateDelayMs(stateDelayMs)
+                .build();
+
+        gameInfo.setGameName(gameName);
+        gameInfo.setHeight(height);
+        gameInfo.setWidth(width);
+
+        connectionService.createNewGamePlayer(username, "127.0.0.1", 8080, SnakesProto.NodeRole.MASTER);
+        SnakesProto.GameState.Coord[] coords = connectionService.searchPlace();
+        connectionService.createNewSnake(coords);
+
+        gameInfo.setGameConfig(gameConfig);
+
+        log.info("Game config " + gameConfig.toString());
+    }
+}
