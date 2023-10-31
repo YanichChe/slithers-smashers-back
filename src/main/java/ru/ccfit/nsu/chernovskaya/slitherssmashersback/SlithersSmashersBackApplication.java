bfc366d28d2cf09dc57e32a871d4171ba2488a25
tc <ychernovskaya@gmail.com>
2023-10-31 17:30:42 +0700

    ad enable annotation

--- src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/SlithersSmashersBackApplication.java
+++ src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/SlithersSmashersBackApplication.java
@@ -2,7 +2,9 @@ package ru.ccfit.nsu.chernovskaya.slitherssmashersback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SlithersSmashersBackApplication {
 
