da6769ba7f376db20611a6587dda8bf9c40524ca
tc <ychernovskaya@gmail.com>
2023-10-31 17:30:05 +0700

    fix bug in MulticastSenderService

--- src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/services/MulticastSenderService.java
+++ src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/services/MulticastSenderService.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.ccfit.nsu.chernovskaya.slitherssmashersback.SnakesProto;
 import ru.ccfit.nsu.chernovskaya.slitherssmashersback.models.GameInfo;
@@ -15,7 +16,7 @@ import java.net.InetAddress;
 
 @Service
 @Log4j2
-public class MulticastSenderService implements Runnable {
+public class MulticastSenderService {
 
     private final GameInfo gameInfo;
     @Value("${multicast.sender.address}")
@@ -29,13 +30,19 @@ public class MulticastSenderService implements Runnable {
         this.gameInfo = gameInfo;
     }
 
-    @Override
-    public void run() {
-        try (DatagramSocket socket = new DatagramSocket()) {
-            while (true) {
+    @Scheduled(fixedRateString = "${multicast.sender.period}")
+    public void sendAnnouncementMsgPeriodic() {
+        if (gameInfo.getGameConfig() != null) {
+
+            try (DatagramSocket socket = new DatagramSocket()) {
                 try {
+                    SnakesProto.GamePlayers gamePlayers = SnakesProto.GamePlayers
+                            .newBuilder()
+                            .addAllPlayers(gameInfo.getGamePlayers())
+                            .build();
+
                     SnakesProto.GameAnnouncement gameAnnouncement = SnakesProto.GameAnnouncement.newBuilder()
-                            .setPlayers(gameInfo.getGamePlayers())
+                            .setPlayers(gamePlayers)
                             .setConfig(gameInfo.getGameConfig())
                             .setCanJoin(gameInfo.isCanJoin())
                             .setGameName(gameInfo.getGameName())
@@ -43,7 +50,7 @@ public class MulticastSenderService implements Runnable {
 
                     SnakesProto.GameMessage.AnnouncementMsg announcementMsg =
                             SnakesProto.GameMessage.AnnouncementMsg.newBuilder()
-                                    .setGames(0, gameAnnouncement)
+                                    .addGames(gameAnnouncement)
                                     .build();
 
                     SnakesProto.GameMessage gameMessage = SnakesProto.GameMessage.newBuilder()
@@ -56,16 +63,16 @@ public class MulticastSenderService implements Runnable {
                     DatagramPacket packet = new DatagramPacket(buf, buf.length,
                             InetAddress.getByName(groupAddress), groupPort);
 
-                    log.info("sent AnnouncementMsg");
+                    log.debug("sent AnnouncementMsg" + announcementMsg.toString());
                     socket.send(packet);
 
-                    Thread.sleep(1000);
-                } catch (IOException | InterruptedException e) {
+                } catch (IOException e) {
                     socket.close();
                 }
+
+            } catch (IOException e) {
+                log.atLevel(Level.ERROR).log(e.getMessage());
             }
-        } catch (IOException e) {
-            log.atLevel(Level.ERROR).log(e.getMessage());
         }
     }
 }
