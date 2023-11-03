28a2875b380423a7ab92d7ece47471ebe760c0ec
tc <ychernovskaya@gmail.com>
2023-11-03 23:55:23 +0700

    корректная логика изменения координат змейки

--- src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/services/GameControlService.java
+++ src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/services/GameControlService.java
@@ -1,11 +1,14 @@
 package ru.ccfit.nsu.chernovskaya.slitherssmashersback.services;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.ccfit.nsu.chernovskaya.slitherssmashersback.SnakesProto;
 import ru.ccfit.nsu.chernovskaya.slitherssmashersback.models.GameInfo;
 
 @Service
+@Log4j2
 public class GameControlService {
 
     private final GameInfo gameInfo;
@@ -15,9 +18,11 @@ public class GameControlService {
     }
 
     @Scheduled(fixedDelay = 1000)
+    @Async
     public void gameStep() {
         if (gameInfo.getSnakes() != null) {
-            for (SnakesProto.GameState.Snake snake : gameInfo.getSnakes()) {
+            for (int i = 0; i < gameInfo.getSnakes().size(); i++) {
+                SnakesProto.GameState.Snake snake = gameInfo.getSnakes().get(i);
                 int headX = snake.getPoints(0).getX();
                 int headY = snake.getPoints(0).getY();
                 SnakesProto.Direction headDirection = snake.getHeadDirection();
@@ -35,13 +40,29 @@ public class GameControlService {
                     newHeadX++;
                 }
 
+                newHeadX = (newHeadX + gameInfo.getWidth()) % gameInfo.getWidth();
+                newHeadY = (newHeadY + gameInfo.getHeight()) % gameInfo.getHeight();
+
                 SnakesProto.GameState.Coord newHeadCoord = SnakesProto.GameState.Coord.newBuilder()
                         .setX(newHeadX)
                         .setY(newHeadY)
                         .build();
 
-                snake.getPointsList().set(0, newHeadCoord);
-                snake.getPointsList().removeLast();
+                SnakesProto.GameState.Snake.Builder modifiedSnakeBuilder = snake.toBuilder();
+                modifiedSnakeBuilder.clearPoints();
+
+                modifiedSnakeBuilder.addPoints(newHeadCoord);
+                for (int j = 0; j < snake.getPointsList().size() - 1; j++) {
+                    SnakesProto.GameState.Coord point = snake.getPoints(j);
+                    modifiedSnakeBuilder.addPoints(point);
+                }
+
+                SnakesProto.GameState.Snake modifiedSnake = modifiedSnakeBuilder.build();
+
+                gameInfo.getSnakes().remove(i);
+                gameInfo.getSnakes().add(i, modifiedSnake);
+
+                log.debug(modifiedSnake.getPointsList());
             }
         }
     }
