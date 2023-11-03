33428f1133df58365cd9ca9d4b0b289ddc0959db
tc <ychernovskaya@gmail.com>
2023-11-03 23:55:50 +0700

    add async

--- src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/SlithersSmashersBackApplication.java
+++ src/main/java/ru/ccfit/nsu/chernovskaya/slitherssmashersback/SlithersSmashersBackApplication.java
@@ -2,9 +2,11 @@ package ru.ccfit.nsu.chernovskaya.slitherssmashersback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class SlithersSmashersBackApplication {
 
